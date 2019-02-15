183bb8b2fff50ce01ea28d72f88ce2d9b83891c7
LittleRichKid <33172945+LittleRichKid@users.noreply.github.com>
2019-02-15 00:20:20 -0700

    Update hub.js

--- hub.js
+++ hub.js
@@ -31,24 +31,26 @@ Bot.on('message', message => {
         };
     };
     
-    if (message.content.startsWith(';ban')) {
-        if (mention) {
-          if (mem) {
-            mem.ban({
-              reason: 'Banned due to violating the rules!',
-            }).then(() => {
-              message.reply(`Successfully banned ${user.tag}`);
-            }).catch(err => {
-              message.reply('I was unable to ban the member');
-              console.error(err);
-            });
-          } else {
-            message.reply('That user isn\'t in this guild!');
-          }
-        } else {
-          message.reply('You didn\'t mention the user to ban!');
-        }
-      }
+  //if (message.content.startsWith(';ban')) {
+       // const user = message.mentions.users.first();
+       // if (user) {
+          //const member = message.guild.member(user);
+          //if (member) {
+            //member.ban({
+              //reason: 'They were bad!',
+            //}).then(() => {
+              //message.reply(`Successfully banned ${user.tag}`);
+            //}).catch(err => {
+              //message.reply('I was unable to ban the member');
+              //console.error(err);
+            //});
+          //} else {
+            //message.reply('That user isn\'t in this guild!');
+          //}
+        //} else {
+          //message.reply('You didn\'t mention the user to ban!');
+        //}
+      //}
     
     if (msg === Prefix + 'shift') {
         embed = new Discord.RichEmbed()
