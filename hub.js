075485dfc91c6b2669baf147cfd9619774b6219e
LittleRichKid <33172945+LittleRichKid@users.noreply.github.com>
2019-02-15 00:25:51 -0700

    Update hub.js

--- hub.js
+++ hub.js
@@ -13,7 +13,7 @@ Bot.on('message', message => {
     const cont = message.content;
     const msg = cont.toLowerCase();
     const chan = message.channel;
-    const mention = message.guild.member.first();
+    const mention = message.guild.users.first();
     const mem = message.member.guild(mention);
     
     if (!message.guild) return;
@@ -30,27 +30,6 @@ Bot.on('message', message => {
             };
         };
     };
-    
-  //if (message.content.startsWith(';ban')) {
-       // const user = message.mentions.users.first();
-       // if (user) {
-          //const member = message.guild.member(user);
-          //if (member) {
-            //member.ban({
-              //reason: 'They were bad!',
-            //}).then(() => {
-              //message.reply(`Successfully banned ${user.tag}`);
-            //}).catch(err => {
-              //message.reply('I was unable to ban the member');
-              //console.error(err);
-            //});
-          //} else {
-            //message.reply('That user isn\'t in this guild!');
-          //}
-        //} else {
-          //message.reply('You didn\'t mention the user to ban!');
-        //}
-      //}
     
     if (msg === Prefix + 'shift') {
         embed = new Discord.RichEmbed()
